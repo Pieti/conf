aa847797c72e1980bcaaa11f114871d1f575c295
Niko Pietikäinen <nikpie@proton.me>
2025-04-17 16:03:10 +0200

    Update nvim install script

--- scripts/install-neovim.sh
+++ scripts/install-neovim.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
+IMAGE_NAME="nvim-linux-x86_64.appimage"
 mkdir -p ~/bin
-curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
-chmod +x nvim.appimage
-mv nvim.appimage ~/bin/nvim
+curl -LO https://github.com/neovim/neovim/releases/latest/download/"${IMAGE_NAME}"
+chmod +x "${IMAGE_NAME}"
+mv "${IMAGE_NAME}" ~/bin/nvim
